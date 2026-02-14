b5fedb52075503b0e0c0a8caff45d034996e9ce6
Skanexis <verdesmassimo@gmail.com>
2026-02-14 22:20:11 +0100

    Fix publish SQL comma and skip empty telegram drafts v2

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json({ limit: '3mb' }));
 
 app.use((req, res, next) => {
  res.setHeader('Access-Control-Allow-Origin', '*');
- res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+ res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
  if (req.method === 'OPTIONS') return res.sendStatus(200);
  next();
